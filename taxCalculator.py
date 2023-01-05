965aa99b590ff1df4c1bbf7406de0c15f2380a03
Ashley-Ung <62251927+Ashley-Ung@users.noreply.github.com>
2023-01-05 15:19:08 -0800

    user prompts

---
+++ taxCalculator.py
@@ -0,0 +1,6 @@
+# Prompt the user to enter their filing status, income, tax deductions, tax exemptions, & tax paid 
+filingStatus = input ("Enter your filing status (single, married filing jointly, married filing separately, head of household, 'widowed'): ")
+combindedIncome = float (input ("Enter your combined (nontaxable and taxable) income: "))
+taxedDeductions = float (input ("Enter your tax deductions: "))
+taxedExemptions = float (input ("Enter your tax exemptions: "))
+taxPaid = float (input ("Enter the amount of tax you have already paid: "))
