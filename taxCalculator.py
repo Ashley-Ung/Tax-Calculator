7cf939342dd037c5a93c17ffccc064050fc89db4
Ashley-Ung <62251927+Ashley-Ung@users.noreply.github.com>
2023-01-05 15:19:53 -0800

    calculate taxable income and tax liability calcs

--- taxCalculator.py
+++ taxCalculator.py
@@ -4,3 +4,45 @@ combindedIncome = float (input ("Enter your combined (nontaxable and taxable) in
 taxedDeductions = float (input ("Enter your tax deductions: "))
 taxedExemptions = float (input ("Enter your tax exemptions: "))
 taxPaid = float (input ("Enter the amount of tax you have already paid: "))
+
+# Calculate the taxable income
+income = combindedIncome - taxedDeductions - taxedExemptions
+
+# Calculate the tax liability based on the filing status and income
+if filingStatus == "single":
+	tax = 0
+	for i in range (len (singleTaxRates)):
+		if income > singleTaxBrackets[i]:
+			tax += (income - singleTaxBrackets[i]) * singleTaxRates[i]
+			income = singleTaxBrackets[i]
+	print ("Your tax liability is: $" + str (tax))
+elif filingStatus == "married filing jointly":
+	tax = 0
+	for i in range (len (jointTaxRates)):
+		if income > jointTaxBrackets[i]:
+			tax += (income - jointTaxBrackets[i]) * jointTaxRates[i]
+			income = jointTaxBrackets[i]
+	print ("Your tax liability is: $" + str (tax))
+elif filingStatus == "married filing separately":
+	tax = 0
+	for i in range (len (separateTaxRates)):
+		if income > separateTaxBrackets[i]:
+			tax += (income - separateTaxBrackets[i]) * separateTaxRates[i]
+			income = separateTaxBrackets[i]
+	print ("Your tax liability is: $" + str (tax))
+elif filingStatus == "head of household":
+	tax = 0
+	for i in range (len (headTaxRates)):
+		if income > headTaxBrackets[i]:
+			tax += (income - headTaxBrackets[i]) * headTaxRates[i]
+			income = headTaxBrackets[i]
+	print ("Your tax liability is: $" + str (tax))
+elif filingStatus == "widowed": 
+	tax = 0
+	for i in range (len (widowTaxRates)):
+		if income > widowTaxBrackets[i]:
+			tax += (income - widowTaxBrackets[i]) * widowTaxRates[i]
+			income = widowTaxBrackets[i]
+	print ("Your tax liability is: $" + str (tax))
+else:
+	print ("Invalid filing status")
